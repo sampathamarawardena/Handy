68e6e72faa4904dbc9a8ced73e18a34b6b0f7122
ArunaAmarawardena <sampath.amarawardena@gmail.com>
2017-05-29 11:59:07 +0530

    Add Layouts

--- app/src/main/java/com/sasoftgroups/handy/HomePage.java
+++ app/src/main/java/com/sasoftgroups/handy/HomePage.java
@@ -1,13 +1,23 @@
 package com.sasoftgroups.handy;
 
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
+
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+
+    public void btnNeedHand(View view) {
+        Intent needHand = new Intent(HomePage.this, HandRequest.class);
+        startActivity(needHand);
     }
 }
