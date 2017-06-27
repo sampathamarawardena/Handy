a1aa409a84e08fe3fcd451bf0e87c0a15cc9d821
ArunaAmarawardena <sampath.amarawardena@gmail.com>
2017-06-27 10:53:59 +0530

    Add User Profile Interface

---
+++ app/src/main/java/com/sasoftgroups/handy/FriednsList.java
@@ -0,0 +1,13 @@
+package com.sasoftgroups.handy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FriednsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friedns_list);
+    }
+}
